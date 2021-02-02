dbaebfeef1f35193a34e20a02f8bcccf36c0e811
elishian <48224412+elishian@users.noreply.github.com>
2021-02-02 12:19:38 -0500

    Add files via upload

---
+++ learnABS.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tues July 21 15:50:35 2020
+
+This is a program that brings up the tabs I'm learning from in
+Automate the Boring Stuff
+Plus
+A more general python tutorial
+http://greenteapress.com/thinkpython2/html/index.html
+
+@author: Eli
+"""
+
+import webbrowser
+
+webbrowser.open('https://automatetheboringstuff.com/2e/chapter12/')
+webbrowser.open('https://automatetheboringstuff.com/2e/chapter17/')
+webbrowser.open('http://greenteapress.com/thinkpython2/html/index.html')
+
+
+
